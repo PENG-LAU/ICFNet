8e08d7fba4f97040c4510f181d4418c76513a387
PENG-LAU <99115407+PENG-LAU@users.noreply.github.com>
2025-05-13 15:57:02 +0800

    Update Block.py

--- model/Block.py
+++ model/Block.py
@@ -74,10 +74,20 @@ class LRSC(nn.Module):
 class LCM(nn.Module):
     def __init__(self, in_features, d_hid, out_features, in_fea, out_fea, kernel_size, stride, drop_path):
         super().__init__()
-        "For safety reasons, it will be made public later."
+        self.mlp = MLP(in_features, d_hid, out_features)
+        self.conv1 = nn.Conv1d(in_fea, out_fea, kernel_size, stride)
+        self.conv2 = nn.Conv1d(in_fea, out_fea, kernel_size, stride)
+
+        self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()  # DropPath：将多分支结构的子路径 "随机删除“
+        self.act_layer = nn.GELU()
 
     def forward(self, x):
-        "For safety reasons, it will be made public later."
+        x_mlp = self.drop_path(self.act_layer(self.mlp(x)))
+
+        t = torch.concat([x, x_mlp], -2)
+        t = t.permute(0, 2, 1)
+        t_conv = self.conv1(t)
+        t_conv = t_conv.permute(0, 2, 1)
         
         return x
 
@@ -152,22 +162,125 @@ class MLP(nn.Module):
 class GG_ACM(nn.Module):
     def __init__(self, adj, dim, d_hid, drop_path, drop, norm_layer=nn.LayerNorm):
         super().__init__()
-        "For safety reasons, it will be made public later."
+        self.index_1 = [0, 1, 4, 7, 8, 9, 10, 11, 14]
+        self.index_r_Hip = [2, 3]
+        self.index_l_Hip = [5, 6]
+        self.index_l_Shoulder = [12, 13]
+        self.index_r_Shoulder = [15, 16]
+
+        self.mlp1 = MLP(dim, d_hid, dim, drop=drop).to(device)
+
+        self.mlp2 = MLP(2 * dim, d_hid, dim, drop=drop).to(device)
+
+        self.norm_layer = norm_layer(dim)
+        self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()  # DropPath：将多分支结构的子路径 "随机删除“
+        self.dim = dim
+
+        self.gelu = nn.GELU()
+
+        self.conv1 = nn.Conv1d(dim, dim, kernel_size=3, padding=4, stride=3)
+
+        self.lrs = LRSC(adj, dim, drop_path=drop_path, norm_layer=norm_layer)
+
 
     def forward(self, x):
-        "For safety reasons, it will be made public later."
-        return x
+        x = self.norm_layer(x)  # （17， dim）
+        x_pooling_r_Hip = x[:, self.index_r_Hip, :]
+        x_pooling_l_Hip = x[:, self.index_l_Hip, :]
+        x_pooling_l_Shoulder = x[:, self.index_l_Shoulder, :]
+        x_pooling_r_Shoulder = x[:, self.index_r_Shoulder, :]
+
+        y1 = x[:, self.index_1, :]  # (batch, 8, dim)
+        # 
+        for i in range(2):
+            y1[:, 1, :] += x_pooling_r_Hip[:, i, :]
+            y1[:, 2, :] += x_pooling_l_Hip[:, i, :]
+            y1[:, 7, :] += x_pooling_l_Shoulder[:, i, :]
+            y1[:, 8, :] += x_pooling_r_Shoulder[:, i, :]
+
+        x1 = self.drop_path(self.gelu(self.mlp1(y1)))  # (9, dim)
+
+        #
+        B, N, C = x1.shape
+        y2 = torch.zeros(B, 2 * N - 1, 2 * C).to(device)
+
+        for i in range(0, 9):
+            if i == 2:
+                y2[:, 2 * i, :] = torch.concat([x[:, 2 * i, :], x[:, 0, :]], -1)
+            elif i == 7:
+                y2[:, 2 * i, :] = torch.concat([x[:, 2 * i, :], x[:, 8, :]], -1)
+            else:
+                y2[:, 2 * i, :] = torch.concat([x[:, 2 * i, :], x[:, 2 * i -1, :]], -1)
+        for i in range(0, 8):
+            if i == 3:
+                y2[:, 2 * i + 1, :] = torch.concat([x[:, 2 * i + 1, :], x[:, 0, :]], -1)
+            elif i == 5:
+                y2[:, 2 * i + 1, :] = torch.concat([x[:, 2 * i + 1, :], x[:, 8, :]], -1)
+            else:
+                y2[:, 2 * i + 1, :] = torch.concat([x[:, 2 * i + 1, :], x[:, 8 + i, :]], -1)
+
+        x2 = self.drop_path(self.gelu(self.mlp2(y2)))  # (17, dim)
+
+        # 
+        y3 = self.mlp1(torch.concat([x, x1, x2], -2))
+        y3 = y3.permute(0, 2, 1)
+        x3 = self.drop_path(self.gelu(self.conv1(y3)))
+        x3 = x3.permute(0, 2, 1)
+
+        x3 = self.drop_path(self.gelu(self.lrs(x3)))  # (17, dim)
+        return x + x2 + x3
 
 
 class RR_ACM(nn.Module):
     def __init__(self, adj, dim, d_hid, drop_path, ):
         super().__init__()
-        "For safety reasons, it will be made public later."
+        self.layer_normal = nn.LayerNorm(dim)
+        self.mlp1 = MLP(2 * dim, d_hid, dim)
+        self.mlp2 = MLP(dim, d_hid, dim)
+
+        self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
+        self.gelu = nn.GELU()
+
+        self.conv1 = nn.Conv1d(dim, dim, kernel_size=3, padding=1, stride=2)
+        self.conv2 = nn.Conv1d(dim, dim, kernel_size=4, padding=1, stride=4)
+
+        self.lrs = LRSC(adj, dim, drop_path=drop_path, norm_layer=nn.LayerNorm)
 
     def forward(self, x):
-        "For safety reasons, it will be made public later."
+        x = self.layer_normal(x)
 
-        return x
+        # 扩大过程
+        B, N, C = x.shape
+        y1 = torch.zeros(B, 2 * N - 1, 2 * C).to(device)
+
+        for i in range(0, 17):
+            y1[:, 2 * i, :] = torch.concat([x[:, i, :], x[:, i, :]], -1)
+        for i in range(0, 16):
+            if i == 3 or i == 6:
+                y1[:, 2 * i + 1, :] = torch.concat([x[:, 0, :], x[:, i + 1, :]], -1)
+            elif i == 10 or i == 13:
+                y1[:, 2 * i + 1, :] = torch.concat([x[:, 8, :], x[:, i + 1, :]], -1)
+            else:
+                y1[:, 2 * i + 1, :] = torch.concat([x[:, i, :], x[:, i + 1, :]], -1)  # 将相邻两行concat
+
+        # print(y1.shape)
+
+        x1 = self.drop_path(self.gelu(self.mlp1(y1)))  # (33, dim)
+
+        # 
+        x1_1 = x1.permute(0, 2, 1)
+        y2 = self.drop_path(self.gelu(self.conv1(x1_1)))
+        y2 = y2.permute(0, 2, 1)
+        x2 = self.drop_path(self.gelu(self.mlp2(y2)))  # (17,dim)
+
+        # 
+        y3 = self.mlp2(torch.concat([x, x1, x2], -2))
+        y3 = y3.permute(0, 2, 1)
+        x3 = self.drop_path(self.gelu(self.conv2(y3)))
+        x3 = x3.permute(0, 2, 1)
+        x3 = self.drop_path(self.gelu(self.lrs(x3)))  # (17, dim)
+
+        return x + x3 + x2
 
 
 class Hiremixer(nn.Module):
